c2de077e54ce5a7ca52e4625babfe683c6bf5128
irgaly <irgaly@gmail.com>
2022-05-21 16:40:25 +0900

    update flutter pubspec

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -2,7 +2,9 @@
 // Generated file. Do not edit.
 //
 
+// ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
 
 import 'package:password_credential/src/password_credential_web.dart';
 
